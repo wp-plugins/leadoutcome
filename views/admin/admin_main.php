297b5919d9c37ae32841bb1241f129c7838a682e
leadoutcome <leadoutcome@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 22:23:16 +0000

    Support for UID and removal of java script code
    git-svn-id: https://plugins.svn.wordpress.org/leadoutcome/trunk@1161525 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/admin/admin_main.php
+++ views/admin/admin_main.php
@@ -4,42 +4,58 @@
 		<td width="100%" style="vertical-align:top;">
 
 <div style="font:13px/20px 'Helvetica Neue',Helvetica,Arial,sans-serif;">
-	<p><a href="http://www.leadoutcome.com" target="_blank" title="<?php echo __('LeadOutcome','lo'); ?>"><img alt="" src="http://768fb7af815f78d7559a-1cf765037453fe7e5aa46c8e17022e63.r47.cf2.rackcdn.com/staging/sites/all/themes/rubik/cube/logo_1336174074.png" alt="<?php echo __('LeadOutcome','lo'); ?>" title="<?php echo __('LeadOutcome','lo'); ?>" style="width: 265px; height: 60px;border:none;" align="middle"></a>&nbsp;&nbsp;<input type="button" class="button-primary" value="<?php _e('View / Manage Opt-in Forms') ?>" onClick="location.href='admin.php?page=leadoutcome_optin_forms';" />&nbsp;&nbsp;&nbsp;<input type="button" class="button-primary" value="<?php _e('Lead Tracking / Conversions Options') ?>" onClick="location.href='admin.php?page=leadoutcome_lead_track_convert_options';" />
+
+	<p><a href="http://www.leadoutcome.com" target="_blank" title="<?php echo __('LeadOutcome','lo'); ?>"><img alt="" src="http://768fb7af815f78d7559a-1cf765037453fe7e5aa46c8e17022e63.r47.cf2.rackcdn.com/staging/sites/all/themes/rubik/cube/logo_1336174074.png" alt="<?php echo __('LeadOutcome','lo'); ?>" title="<?php echo __('LeadOutcome','lo'); ?>" style="width: 265px; height: 60px;border:none;" align="middle"></a>&nbsp;&nbsp;
+    &nbsp;&nbsp;
+    	<input type="submit" class="button-primary" value="<?php _e('Account Setup') ?>" onClick="location.href='admin.php?page=leadoutcome_lead_track_convert_options';" />&nbsp;&nbsp;&nbsp;<input type="submit" class="button-primary" value="<?php _e('View / Manage Opt-in Forms') ?>" onClick="location.href='admin.php?page=leadoutcome_optin_forms';" />
 	</p>
 	<p>&nbsp;</p>
 
 	<p><img alt="" height="140" src="http://768fb7af815f78d7559a-1cf765037453fe7e5aa46c8e17022e63.r47.cf2.rackcdn.com/staging/sites/all/files/bnr_430_90_turbo_charge_1337039016.jpg" width="672"></p>
+
 	<p>Are you ready to turbo charge your WordPress site by incorporating LeadOutcome's all-in-one marketing system?</p>
+
 	<p>After your plugin has been installed, you will be able to easily capture leads and track and score them as they read your blog postings and visit various pages on your site.
+
 	</p>
+
 	<p>With yourWordPress website,you will be able to take full advantage of LeadOutcome's other great <a href="http://www.leadoutcome.com/pub/feature-comparison" style="color:#CC8315;font-weight:bold;text-decoration:underline;" target="_blank">features</a>.</p>
+
 	<h1 style="font-weight:18px;color:#85AC3D;font-weight:bold;font-family:'Lucida','Lucida Grande','Trebuchet MS',Tahoma,Verdana,sans-serif">
+
 	Getting Started</h1>
+
 	<p>The process for enabling your WordPress site with LeadOutcome is simple:<br>
+
 	<br>
+
 	<ol>
+
 		<li>Install the LeadOutcome WordPress plugin into your WordPress site. You can accomplish this by going to the WordPress plugin search facility and search for the term LeadOutcome. </li>
+
 		<li>Make sure to activate the plugin</li>
+
 		<li>Enable Tracking/Scoring feature for all web pages, posts or both.</li>
+
 		<li>If you don't already have one, create a LeadOutcome Account. (<a href="http://www.leadoutcome.com/pub/getting-started" style="color:#CC8315;font-weight:bold;text-decoration:underline;" target="_blank">Sign Up</a>)</li>
+
 		<li>Generate an Opt-In form from one of our professional templates using our Opt-In Form Wizard. The wizard can be located by going to "Marketing Sites &gt;&gt; Settings &gt;&gt; External Web Sites &gt;&gt; Opt-In Form Wizard". Generate your form and copy it to the paste board.</li>
+
 		<li>Switch to your WordPress site and Select "LeadOutcome &gt;&gt; Opt-In Form" menu</li>
+
 		<li>Add a new opt-in form and paste your generated form into the text field and save.</li>
+
 		<li>Copy and paste the shortcode into the page or post you would like to see the Opt-In form on.</li>
+
 	</ol>
+
 	<p>Thats it! Next,watch for email notifications from LeadOutcome when you receive new leads. Login to LeadOutcome and checkout the score and history of your leads. See what blog postings interest them and what pages they are visiting. Thereafter, send them a 1-click email or assign them to an email campaign.</p>
 	<p>To discover evenmore ways you canleverage this marketing system, click <a href="https://www1.gotomeeting.com/register/329941057" style="color:#CC8315;font-weight:bold;text-decoration:underline;" target="_blank">HERE</a> to register for our weekly webinar.</p>
-
-	<p class="submit"><input type="submit" class="button-primary" value="<?php _e('View / Manage Opt-in Forms') ?>" onClick="location.href='admin.php?page=leadoutcome_optin_forms';" />&nbsp;&nbsp;&nbsp;<input type="submit" class="button-primary" value="<?php _e('Lead Tracking / Conversions Options') ?>" onClick="location.href='admin.php?page=leadoutcome_lead_track_convert_options';" /></p>
-
-
+	<p class="submit"><input type="submit" class="button-primary" value="<?php _e('Account Setup') ?>" onClick="location.href='admin.php?page=leadoutcome_lead_track_convert_options';" />&nbsp;&nbsp;&nbsp;<input type="submit" class="button-primary" value="<?php _e('View / Manage Opt-in Forms') ?>" onClick="location.href='admin.php?page=leadoutcome_optin_forms';" /></p>
 </div>
-
-
 		</td>
 	</tr>
 	</table>
-
   </form>
-
 </div>
+
--- views/admin/admin_optin_forms.php
+++ views/admin/admin_optin_forms.php
@@ -1,38 +1,76 @@
 <div class="wrap">
 
-	<p><a href="http://www.leadoutcome.com" target="_blank" title="<?php echo __('LeadOutcome','lo'); ?>"><img alt="" src="http://768fb7af815f78d7559a-1cf765037453fe7e5aa46c8e17022e63.r47.cf2.rackcdn.com/staging/sites/all/themes/rubik/cube/logo_1336174074.png" alt="<?php echo __('LeadOutcome','lo'); ?>" title="<?php echo __('LeadOutcome','lo'); ?>" style="width: 265px; height: 60px;border:none;" align="middle"></a>&nbsp;&nbsp;<input type="button" class="button-primary" value="<?php _e('View / Manage Opt-in Forms') ?>" onClick="location.href='admin.php?page=leadoutcome_optin_forms';" />&nbsp;&nbsp;&nbsp;<input type="button" class="button-primary" value="<?php _e('Lead Tracking / Conversions Options') ?>" onClick="location.href='admin.php?page=leadoutcome_lead_track_convert_options';" />
+
+
+	<p><a href="http://www.leadoutcome.com" target="_blank" title="<?php echo __('LeadOutcome','lo'); ?>"><img alt="" src="http://768fb7af815f78d7559a-1cf765037453fe7e5aa46c8e17022e63.r47.cf2.rackcdn.com/staging/sites/all/themes/rubik/cube/logo_1336174074.png" alt="<?php echo __('LeadOutcome','lo'); ?>" title="<?php echo __('LeadOutcome','lo'); ?>" style="width: 265px; height: 60px;border:none;" align="middle"></a>&nbsp;&nbsp;<input type="submit" class="button-primary" value="<?php _e('Account Setup') ?>" onClick="location.href='admin.php?page=leadoutcome_lead_track_convert_options';" />&nbsp;&nbsp;&nbsp;<input type="submit" class="button-primary" value="<?php _e('View / Manage Opt-in Forms') ?>" onClick="location.href='admin.php?page=leadoutcome_optin_forms';" />
+
 	</p>
+
 	<p>&nbsp;</p>
+
+
+
 
 
 	<h2><?php _e('LeadOutcome - Opt-In Forms','lo'); ?></h2>
 
+
+
 	<?php
+
 	if(isset($_REQUEST['deleted']) && $_REQUEST['deleted'])
+
 	{
+
 		lo_messages(__('Selected Forms have been deleted','lo'),'message');
+
 	}
+
 	?>
 
+
+
 	<?php if(current_user_can('administrator')) { ?><h3><a href="#add_optin_form"><?php _e('Add New Opt-In Form','lo'); ?></a></h3><?php } ?>
+
 	<!-- Forms are NOT created automatically, so you need to wrap the table in one to use features like bulk actions -->
+
 	<form id="forms-filter" method="get">
+
 		<!-- For plugins, we also need to ensure that the form posts back to our current page -->
+
 		<input type="hidden" name="page" value="<?php echo $_REQUEST['page'] ?>" />
+
 		<!-- Now we can render the completed list table -->
+
 		<h4 style="color:green;font-style:italic;">Copy and paste the shortcode(s) you want on your posts and pages to display the respective form</h4>
+
 		<?php $LeadOutcome_OptinForms_List_Table->display() ?>
+
 		<h4 style="color:green;font-style:italic;">Copy and paste the shortcode(s) you want on your posts and pages to display the respective form</h4>
+
 	</form>
+
 </div>
 
+
+
 <br />
+
 <br />
+
 <hr />
+
 <br />
+
 <br />
+
 <?php if(current_user_can('administrator')) { ?>
+
 	<a id="add_optin_form" />
+
 	<?php $add_optin_form_included = true; include_once( LO_PLUGIN_DIR . '/views/admin/form_add_optin_form.php' ); ?>
+
 <?php
+
 }
+
