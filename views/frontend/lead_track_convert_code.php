66e7b7a92a1abbb382f50b26d301e577f2d2b35c
leadoutcome <leadoutcome@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-15 23:55:04 +0000

    Support for UID and removal of java script code
    git-svn-id: https://plugins.svn.wordpress.org/leadoutcome/trunk@1161576 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- views/frontend/lead_track_convert_code.php
+++ views/frontend/lead_track_convert_code.php
@@ -1,4 +1,4 @@
 <!--START SMART PIXEL - DO NOT EDIT-->
-<img width="1" height="1" src="https://www.leadoutcome.com/track/image?uid=<?php echo $uid; ?>&activity=<?php echo $lo_lead_track_convert_activity; ?>&eventDetail=<?php echo $lo_this_page_visited_title; ?>&a1click=&incrementScore=5&type=LEAD&transferLead="/>
+<img width="1" height="1" src="https://www.leadoutcome.com/track/image?uid=<?php echo lo_get_uid(); ?>&activity=<?php echo $lo_lead_track_convert_activity; ?>&eventDetail=<?php echo $lo_this_page_visited_title; ?>&a1click=&incrementScore=5&type=LEAD&transferLead="/>
 <!-- END SMART PIXEL-->  
 
